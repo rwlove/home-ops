368c37b2fe8fb1ac7396355579ffdc08f22d35c4
Robert Love <terp4life2001@gmail.com>
2021-05-02 16:43:14 -0400

    change-env_vars-path

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-. ../env_vars.txt
+. ./env_vars.txt
 
 flux bootstrap github --verbose --owner=$GITHUB_USER --repository=fleet-infra --branch=main --path=./clusters/lovenet --personal

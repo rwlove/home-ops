1b6efb6a838d37aa7f79d350e76b6539f40a2455
Robert Love <terp4life2001@gmail.com>
2021-05-03 17:41:56 -0400

    fix-lint

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 
 . ./env_vars.txt
 
-flux bootstrap github --verbose --owner=$GITHUB_USER --repository=fleet-infra --branch=main --path=./clusters/lovenet --personal --log-level debug --verbose
+flux bootstrap github --verbose --owner=$GITHUB_USER --repository=fleet-infra --branch=main --path=./clusters/lovenet/base --personal --log-level debug --verbose

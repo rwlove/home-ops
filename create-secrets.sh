caf40d4a6e009cb64c372628b842cb7e7606cbd1
Robert Love <terp4life2001@gmail.com>
2021-05-05 13:19:31 -0400

    fix-hass-secrets

--- create-secrets.sh
+++ create-secrets.sh
@@ -8,6 +8,7 @@ envsubst < ./tmpl/.sops.yaml > ./.sops.yaml
 envsubst < ./tmpl/cluster-secrets.yaml > ./clusters/lovenet/base/cluster-secrets.yaml
 envsubst < ./tmpl/cluster-settings.yaml > ./clusters/lovenet/base/cluster-settings.yaml
 envsubst < ./tmpl/gotk-sync.yaml > ./clusters/lovenet/base/flux-system/gotk-sync.yaml
+envsubst < ./clusters/lovenet/apps/home/home-assistant/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
 
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 
@@ -15,5 +16,5 @@ sops --encrypt ./clusters/lovenet/core/monitoring/grafana/tmpl/secrets-tmpl.yaml
 sops --encrypt ./clusters/lovenet/core/monitoring/prometheus/tmpl/secrets-tmpl.yaml  > ./clusters/lovenet/core/monitoring/prometheus/secrets.yaml
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/secrets-tmpl.yaml           > ./clusters/lovenet/apps/media/kodidb/secrets.yaml
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/mysql-env-secrets-tmpl.yaml > ./clusters/lovenet/apps/media/kodidb/mysql-env.secret.yaml
-sops --encrypt ./clusters/lovenet/apps/home/home-assistant/tmpl/secrets-tmpl.yaml    > ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
 sops --encrypt ./clusters/lovenet/apps/media/flood/tmpl/secrets-tmpl.yaml            > ./clusters/lovenet/apps/media/flood/secrets.yaml

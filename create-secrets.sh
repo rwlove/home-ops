ff4b535b6e351ce1b5e24f8b03ab705088a4b78b
Robert Love <terp4life2001@gmail.com>
2021-05-05 13:09:36 -0400

    fix-flood-secrets

--- create-secrets.sh
+++ create-secrets.sh
@@ -11,8 +11,9 @@ envsubst < ./tmpl/gotk-sync.yaml > ./clusters/lovenet/base/flux-system/gotk-sync
 
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 
-sops --encrypt ./clusters/lovenet/core/monitoring/grafana/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/core/monitoring/grafana/secrets.yaml
-sops --encrypt ./clusters/lovenet/core/monitoring/prometheus/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/core/monitoring/prometheus/secrets.yaml
-sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/media/kodidb/secrets.yaml
-sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/mysql-env-secrets-tmpl.yaml > clusters/lovenet/apps/media/kodidb/mysql-env.secret.yaml
-sops --encrypt ./clusters/lovenet/apps/home/home-assistant/tmpl/secrets-tmpl.yaml > clusters/lovenet/apps/home/home-assistant/secrets.yaml
+sops --encrypt ./clusters/lovenet/core/monitoring/grafana/tmpl/secrets-tmpl.yaml     > ./clusters/lovenet/core/monitoring/grafana/secrets.yaml
+sops --encrypt ./clusters/lovenet/core/monitoring/prometheus/tmpl/secrets-tmpl.yaml  > ./clusters/lovenet/core/monitoring/prometheus/secrets.yaml
+sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/secrets-tmpl.yaml           > ./clusters/lovenet/apps/media/kodidb/secrets.yaml
+sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/mysql-env-secrets-tmpl.yaml > ./clusters/lovenet/apps/media/kodidb/mysql-env.secret.yaml
+sops --encrypt ./clusters/lovenet/apps/home/home-assistant/tmpl/secrets-tmpl.yaml    > ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
+sops --encrypt ./clusters/lovenet/apps/media/flood/tmpl/secrets-tmpl.yaml            > ./clusters/lovenet/apps/media/flood/secrets.yaml

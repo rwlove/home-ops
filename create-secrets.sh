f331dcf8cca274501084410feacf89a475f84352
Robert Love <terp4life2001@gmail.com>
2021-05-07 14:42:59 -0400

    use-kube-prometheus-stack

--- create-secrets.sh
+++ create-secrets.sh
@@ -12,7 +12,6 @@ envsubst < ./clusters/lovenet/apps/home/home-assistant/tmpl/secrets-tmpl.yaml > 
 
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 
-sops --encrypt ./clusters/lovenet/core/monitoring/prometheus/tmpl/secrets-tmpl.yaml  > ./clusters/lovenet/core/monitoring/prometheus/secrets.yaml
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/secrets-tmpl.yaml           > ./clusters/lovenet/apps/media/kodidb/secrets.yaml
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/mysql-env-secrets-tmpl.yaml > ./clusters/lovenet/apps/media/kodidb/mysql-env.secret.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/home/home-assistant/secrets.yaml

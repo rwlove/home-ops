1c10499d9446b58c90c89b446b5c3f1a5ef51288
Robert Love <terp4life2001@gmail.com>
2021-06-04 19:02:43 -0400

    fix-traefik

--- create-secrets.sh
+++ create-secrets.sh
@@ -10,6 +10,7 @@ envsubst < ./tmpl/cluster-settings.yaml > ./clusters/lovenet/base/cluster-settin
 envsubst < ./tmpl/gotk-sync.yaml > ./clusters/lovenet/base/flux-system/gotk-sync.yaml
 envsubst < ./clusters/lovenet/apps/home/home-assistant/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
 envsubst < ./clusters/lovenet/core/notifications/github/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/core/notifications/github/secrets.yaml
+envsubst < ./clusters/lovenet/apps/network/traefik/middlewares/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/network/traefik/middlewares/secrets.yaml
 
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/secrets-tmpl.yaml           > ./clusters/lovenet/apps/media/kodidb/secrets.yaml
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/mysql-env-secrets-tmpl.yaml > ./clusters/lovenet/apps/media/kodidb/mysql-env.secret.yaml
@@ -17,3 +18,4 @@ sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/mysql-env-secrets-tmpl.
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/core/notifications/github/secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/apps/network/traefik/middlewares/secrets.yaml

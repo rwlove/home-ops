13c33fa605045d42da0366773acfd9a734d5c2aa
Robert Love <terp4life2001@gmail.com>
2021-07-24 06:48:06 -0400

    mullvad-config-secret

--- create-secrets.sh
+++ create-secrets.sh
@@ -15,6 +15,7 @@ envsubst < ./clusters/lovenet/apps/network/traefik/middlewares/tmpl/secrets-tmpl
 
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/secrets-tmpl.yaml           > ./clusters/lovenet/apps/media/kodidb/secrets.yaml
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/mysql-env-secrets-tmpl.yaml > ./clusters/lovenet/apps/media/kodidb/mysql-env.secret.yaml
+sops --encrypt ./clusters/lovenet/apps/media/qbittorrent/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/media/qbittorrent/secrets.yaml
 
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/home/home-assistant/secrets.yaml

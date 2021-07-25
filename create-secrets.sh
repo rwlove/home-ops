8cf039002b5e981e25cca3818778058bd7b1a2bf
Robert Love <terp4life2001@gmail.com>
2021-07-25 16:54:07 -0400

    fix-external-dns-secrets

--- create-secrets.sh
+++ create-secrets.sh
@@ -13,11 +13,11 @@ envsubst < ./clusters/lovenet/apps/home/appdaemon/tmpl/secrets-tmpl.yaml > ./clu
 envsubst < ./clusters/lovenet/core/notifications/github/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/core/notifications/github/secrets.yaml
 envsubst < ./clusters/lovenet/apps/network/traefik/middlewares/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/network/traefik/middlewares/secrets.yaml
 envsubst < ./clusters/lovenet/core/cert-manager/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/core/cert-manager/secrets.yaml
+envsubst < ./clusters/lovenet/apps/network/external-dns/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/network/external-dns/secrets.yaml
 
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/secrets-tmpl.yaml           > ./clusters/lovenet/apps/media/kodidb/secrets.yaml
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/mysql-env-secrets-tmpl.yaml > ./clusters/lovenet/apps/media/kodidb/mysql-env.secret.yaml
 sops --encrypt ./clusters/lovenet/apps/media/qbittorrent/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/media/qbittorrent/secrets.yaml
-sops --encrypt ./clusters/lovenet/apps/network/external-dns/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/network/external-dns/secrets.yaml
 
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
@@ -25,3 +25,4 @@ sops --encrypt --in-place ./clusters/lovenet/apps/home/appdaemon/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/core/notifications/github/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/network/traefik/middlewares/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/core/cert-manager/secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/apps/network/external-dns/secrets.yaml

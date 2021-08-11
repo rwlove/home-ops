92ddaa97bb9d7721b1db0b35ec1643f503302e56
Robert Love <terp4life2001@gmail.com>
2021-08-11 18:25:19 -0400

    use-secret-for-stash-media-path

--- create-secrets.sh
+++ create-secrets.sh
@@ -9,6 +9,7 @@ envsubst < ./tmpl/cluster-secrets.yaml > ./clusters/lovenet/base/cluster-secrets
 envsubst < ./tmpl/cluster-settings.yaml > ./clusters/lovenet/base/cluster-settings.yaml
 envsubst < ./tmpl/gotk-sync.yaml > ./clusters/lovenet/base/flux-system/gotk-sync.yaml
 envsubst < ./clusters/lovenet/apps/home/home-assistant/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
+envsubst < ./clusters/lovenet/apps/media/stash/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/media/stash/secrets.yaml
 envsubst < ./clusters/lovenet/apps/home/appdaemon/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/appdaemon/secrets.yaml
 envsubst < ./clusters/lovenet/core/notifications/github/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/core/notifications/github/secrets.yaml
 envsubst < ./clusters/lovenet/apps/network/traefik/middlewares/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/network/traefik/middlewares/secrets.yaml
@@ -21,6 +22,7 @@ sops --encrypt ./clusters/lovenet/apps/media/qbittorrent/tmpl/secrets-tmpl.yaml 
 
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/apps/media/stash/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/home/appdaemon/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/core/notifications/github/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/network/traefik/middlewares/secrets.yaml

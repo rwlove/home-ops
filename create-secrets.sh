f646025d6243fe48ad18e661f163c2e8edb51a7c
Robert Love <terp4life2001@gmail.com>
2021-08-12 12:47:28 -0400

    provide-secret-for-github-webhook

--- create-secrets.sh
+++ create-secrets.sh
@@ -9,7 +9,7 @@ envsubst < ./tmpl/cluster-secrets.yaml > ./clusters/lovenet/base/cluster-secrets
 envsubst < ./tmpl/cluster-settings.yaml > ./clusters/lovenet/base/cluster-settings.yaml
 envsubst < ./tmpl/gotk-sync.yaml > ./clusters/lovenet/base/flux-system/gotk-sync.yaml
 envsubst < ./clusters/lovenet/apps/home/home-assistant/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
-envsubst < ./clusters/lovenet/apps/flux-system/webhook/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/flux-system/webhook/secrets.yaml
+envsubst < ./clusters/lovenet/apps/flux-system/webhook/github/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/flux-system/webhook/github/secrets.yaml
 envsubst < ./clusters/lovenet/apps/home/appdaemon/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/appdaemon/secrets.yaml
 envsubst < ./clusters/lovenet/core/notifications/github/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/core/notifications/github/secrets.yaml
 envsubst < ./clusters/lovenet/apps/network/traefik/middlewares/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/network/traefik/middlewares/secrets.yaml
@@ -22,7 +22,7 @@ sops --encrypt ./clusters/lovenet/apps/media/qbittorrent/tmpl/secrets-tmpl.yaml 
 
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
-sops --encrypt --in-place ./clusters/lovenet/apps/flux-system/webhook/secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/apps/flux-system/webhook/github/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/home/appdaemon/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/core/notifications/github/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/network/traefik/middlewares/secrets.yaml

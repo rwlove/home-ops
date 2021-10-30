dfca7f94c74b32d534a0f8397fc2426c2a7a4be5
Robert Love <terp4life2001@gmail.com>
2021-10-30 08:33:01 -0400

    fix-kodidb-mariadb-init

--- create-secrets.sh
+++ create-secrets.sh
@@ -18,7 +18,6 @@ envsubst < ./clusters/lovenet/apps/network/external-dns/tmpl/secrets-tmpl.yaml >
 envsubst < ./clusters/lovenet/apps/home/nextcloud/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/nextcloud/secrets.yaml
 
 sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/secrets-tmpl.yaml           > ./clusters/lovenet/apps/media/kodidb/secrets.yaml
-sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/mysql-env-secrets-tmpl.yaml > ./clusters/lovenet/apps/media/kodidb/mysql-env.secret.yaml
 sops --encrypt ./clusters/lovenet/apps/media/qbittorrent/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/media/qbittorrent/secrets.yaml
 
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml

c7d93bd14b6b70c89c7640cdec79de0be73929c7
Robert Love <terp4life2001@gmail.com>
2022-02-07 13:50:53 -0500

    update-secrets

--- initialize-cluster.sh
+++ initialize-cluster.sh
@@ -13,10 +13,13 @@ kubectl create secret generic sops-gpg \
     --namespace=flux-system \
     --from-file=sops.asc=/dev/stdin
 
-echo "######"
-echo "# 1st Application"
-kubectl apply --kustomize=./clusters/lovenet/base/flux-system
+#echo "######"
+#echo "# 1st Application"
+#kubectl apply --kustomize=./clusters/lovenet/base/flux-system
 
-echo "######"
-echo "# 2nd Application"
-kubectl apply --kustomize=./clusters/lovenet/base/flux-system
+#echo "######"
+#echo "# 2nd Application"
+#kubectl apply --kustomize=./clusters/lovenet/base/flux-system
+
+# Bootstrap
+./bootstrap.sh

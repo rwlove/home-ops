c24d60ade8ec600a37f852333718ceb647549461
Robert Love <terp4life2001@gmail.com>
2022-03-28 10:07:46 -0400

    remove-tmpl-secrets

--- create-secrets.sh
+++ create-secrets.sh
@@ -6,6 +6,7 @@ export GPG_TTY=$(tty)
 
 # Cluster Secrets
 envsubst < ./tmpl/cluster-secrets.yaml > ./clusters/lovenet/base/cluster-secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 
 # Frigate Secrets
 envsubst < ./clusters/lovenet/apps/home/frigate/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/frigate/secrets.yaml
@@ -15,25 +16,25 @@ sops --encrypt --in-place ./clusters/lovenet/apps/home/frigate/secrets.yaml
 envsubst < ./clusters/lovenet/apps/home/home-assistant/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
 
-
-
-
-envsubst < ./clusters/lovenet/apps/flux-system/webhook/github/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/flux-system/webhook/github/secrets.yaml
-envsubst < ./clusters/lovenet/apps/home/appdaemon/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/appdaemon/secrets.yaml
-envsubst < ./clusters/lovenet/core/notifications/github/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/core/notifications/github/secrets.yaml
-envsubst < ./clusters/lovenet/apps/network/traefik/middlewares/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/network/traefik/middlewares/secrets.yaml
-envsubst < ./clusters/lovenet/apps/network/external-dns/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/network/external-dns/secrets.yaml
-envsubst < ./clusters/lovenet/apps/cert-manager/issuers/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/cert-manager/issuers/secrets.yaml
-
-sops --encrypt ./clusters/lovenet/apps/media/kodidb/tmpl/secrets-tmpl.yaml           > ./clusters/lovenet/apps/media/kodidb/secrets.yaml
-sops --encrypt ./clusters/lovenet/apps/media/qbittorrent/tmpl/secrets-tmpl.yaml > ./clusters/lovenet/apps/media/qbittorrent/secrets.yaml
-
-sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
-sops --encrypt --in-place ./clusters/lovenet/apps/home/home-assistant/secrets.yaml
+# Github Webhook Secrets
+envsubst < ./clusters/lovenet/apps/flux-system/webhook/github/secrets-tmpl.yaml > ./clusters/lovenet/apps/flux-system/webhook/github/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/flux-system/webhook/github/secrets.yaml
-sops --encrypt --in-place ./clusters/lovenet/apps/home/appdaemon/secrets.yaml
-sops --encrypt --in-place ./clusters/lovenet/core/notifications/github/secrets.yaml
-sops --encrypt --in-place ./clusters/lovenet/apps/network/traefik/middlewares/secrets.yaml
-sops --encrypt --in-place ./clusters/lovenet/apps/network/external-dns/secrets.yaml
-sops --encrypt --in-place ./clusters/lovenet/apps/cert-manager/issuers/secrets.yaml
 
+# Appademon Secrets
+envsubst < ./clusters/lovenet/apps/home/appdaemon/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/appdaemon/secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/apps/home/appdaemon/secrets.yaml
+
+# Github Notification Secrets
+envsubst < ./clusters/lovenet/core/notifications/github/secrets-tmpl.yaml > ./clusters/lovenet/core/notifications/github/secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/core/notifications/github/secrets.yaml
+
+# External DNS Secrets
+envsubst < ./clusters/lovenet/apps/network/external-dns/secrets-tmpl.yaml > ./clusters/lovenet/apps/network/external-dns/secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/apps/network/external-dns/secrets.yaml
+
+# Kodi Secrets
+envsubst < ./clusters/lovenet/apps/media/kodidb/secrets-tmpl.yaml > ./clusters/lovenet/apps/media/kodidb/secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/apps/media/kodidb/secrets.yaml
+
+# Qbittorrent Secrets (TODO: move vpn configuration into .cluster-secrets.yaml so that secrets-tmpl.yaml can be committed)
+sops --encrypt ./clusters/lovenet/apps/media/qbittorrent/secrets-tmpl.yaml > ./clusters/lovenet/apps/media/qbittorrent/secrets.yaml

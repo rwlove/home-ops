af07d7862983592f299837f0734febb9e480783a
Robert Love <terp4life2001@gmail.com>
2022-09-13 11:56:42 -0400

    update-gotk-sync-version

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,7 @@
 #. ./env_vars.txt
 . .cluster-secrets.env
 
+echo "GITHUB_USER: ${GITHUB_USER}"
+echo "GITHUB_TOKEN: ${GITHUB_TOKEN}"
+
 flux bootstrap github --verbose --owner=$GITHUB_USER --repository=fleet-infra --branch=main --path=./clusters/lovenet/base --personal --log-level debug

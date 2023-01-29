7a9e2eb2350b2c7f6f47b39e9f21a0b0c9515c9d
Robert Love <terp4life2001@gmail.com>
2023-01-29 11:44:39 -0500

    update-secrets

--- tools/stop-longhorn-volumes.sh
+++ tools/stop-longhorn-volumes.sh
@@ -3,19 +3,20 @@
 
 kubectl -n home scale --replicas=0 deploy/frigate
 kubectl -n home scale --replicas=0 deploy/hajimari
+kubectl -n home scale --replicas=0 deploy/double-take
 
 kubectl -n media scale --replicas=0 deploy/gonic
-kubectl -n media scale --replicas=0 deploy/kodi-helm-kodi-matrix
 kubectl -n media scale --replicas=0 deploy/rompr
 kubectl -n media scale --replicas=0 deploy/stash
 kubectl -n media scale --replicas=0 deploy/youtubedl-material
+kubectl -n media scale --replicas=0 deploy/photoprism
 
 kubectl -n monitoring scale --replicas=0 deploy/statping deploy/portainer deploy/netdata-parent
 
 kubectl -n collab scale --replicas=0 deploy/vikunja
 
 # Stateful Sets
-kubectl -n media scale --replicas=0 statefulsets.apps kodidb-helm-mariadb whisparr lidarr sonarr radarr airsonic
-kubectl -n home scale --replicas=0 statefulsets.apps home-assistant emqx zwavejs2mqtt-z-stick-7 zwavejs2mqtt node-red
+kubectl -n media scale --replicas=0 statefulsets.apps whisparr lidarr sonarr radarr airsonic kodi-nexus kodidb-nexus-mariadb
+kubectl -n home scale --replicas=0 statefulsets.apps home-assistant emqx zwavejs2mqtt-z-stick-7 zwavejs2mqtt node-red esphome barcode-buddy grocy vaultwarden
 kubectl -n downloads scale --replicas=0 statefulsets.apps prowlarr
 kubectl -n databases scale --replicas=0 statefulsets.apps redis-master redis-replicas influxdb-influxdb2

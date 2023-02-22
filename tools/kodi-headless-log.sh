5ed13e18f02b31a01c7e490602ed9828616f5c05
Robert Love <terp4life2001@gmail.com>
2023-02-22 09:14:20 -0500

    disable-vikunja

--- tools/kodi-headless-log.sh
+++ tools/kodi-headless-log.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl -n media exec -i -t `kubectl -n media get pods -l app.kubernetes.io/instance=kodi-helm | grep kodi-helm | cut -d ' ' -f 1` -- tail -F /config/.kodi/temp/kodi.log
+kubectl -n media exec -i -t `kubectl -n media get pods -l app.kubernetes.io/instance=kodi-nexus | grep kodi-nexus | cut -d ' ' -f 1` -- tail -F /config/.kodi/temp/kodi.log

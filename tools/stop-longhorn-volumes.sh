ad94664a5fea08c9fe4c466ece6090f8910e2d09
Robert Love <terp4life2001@gmail.com>
2023-03-26 08:05:44 -0400

    update-stop-longhorn

--- tools/stop-longhorn-volumes.sh
+++ tools/stop-longhorn-volumes.sh
@@ -9,7 +9,7 @@ kubectl -n monitoring scale --replicas=0 deployment statping portainer netdata-p
 kubectl -n collab scale --replicas=0 deployment vikunja
 
 # Stateful Sets
-kubectl -n media scale --replicas=0 statefulsets whisparr lidarr sonarr radarr airsonic kodi-nexus kodidb-nexus-mariadb jellyfin kodi-mariadb
+kubectl -n media scale --replicas=0 statefulsets whisparr lidarr sonarr radarr airsonic kodi-nexus kodidb-nexus-mariadb jellyfin kodi-mariadb sabnzbd
 kubectl -n home scale --replicas=0 statefulsets home-assistant emqx zwavejs2mqtt-z-stick-7 zwavejs2mqtt node-red esphome barcode-buddy grocy vaultwarden frigate zigbee2mqtt
 kubectl -n downloads scale --replicas=0 statefulsets prowlarr
 kubectl -n databases scale --replicas=0 statefulsets redis-master redis-replicas influxdb-influxdb2

5ec30e4146a2615b3726c31f653ac8f7b0093ff4
Robert Love <terp4life2001@gmail.com>
2023-04-03 16:44:29 -0400

    improve-destroy-cluster

--- init/destroy-cluster.sh
+++ init/destroy-cluster.sh
@@ -11,21 +11,16 @@ for node in worker1.thesteamedcrab.com \
         worker6.thesteamedcrab.com \
         worker7.thesteamedcrab.com \
         worker8.thesteamedcrab.com \
-         master3.thesteamedcrab.com \
+        master3.thesteamedcrab.com \
         master2.thesteamedcrab.com \
         master1.thesteamedcrab.com \
           ; do
-    #echo "#### Drain $worker ####"
-    #kubectl drain $node --delete-emptydir-data --force --ignore-daemonsets
+    echo "## $node ## kubectl drain $node --delete-emptydir-data --force --ignore-daemonsets"
+    kubectl drain $node --delete-emptydir-data --force --ignore-daemonsets
 
-    echo "#### Delete $node ####"
+    echo "## $node ## kubectl delete node $node"
     kubectl delete node $node
 done
-
-if [ -d ${HOME}/.kube ] ; then
-    echo "#### Delete ${HOME}/.kube since it exists ####"
-    rm -rf ${HOME}/.kube
-fi
 
 #         worker4.thesteamedcrab.com \
 for node in worker1.thesteamedcrab.com \
@@ -35,20 +30,38 @@ for node in worker1.thesteamedcrab.com \
           worker6.thesteamedcrab.com \
           worker7.thesteamedcrab.com \
           worker8.thesteamedcrab.com \
-           master3.thesteamedcrab.com \
+          master3.thesteamedcrab.com \
           master2.thesteamedcrab.com \
           master1.thesteamedcrab.com \
           ; do
-    echo "#### Reset $node ####"
+    echo "## $node ## ${reset_cmd} ##"
     ssh $node "$reset_cmd"
-    echo "#### rm -rf ~/.kube, /etc/cni, /etc/kubernetes, /var/lib/kubelet, /var/lib/etcd on $node ####"
-    ssh $node "rm -rf ~/.kube/ /etc/cni /etc/kubernetes/ /var/lib/kubelet/ /var/lib/etcd/"
+    
+    echo "## $node ## rm -rf ~/.kube"
+    ssh $node "rm -rf ~/.kube/"
 
-    echo "#### Clear iptables $node ####"
+    echo "## $node ## rm -rf /etc/cni/"
+    ssh $node "rm -rf /etc/cni/"
+
+    echo "## $node ## rm -rf /etc/kubernetes/"
+    ssh $node "rm -rf /etc/kubernetes/"
+
+    echo "## $node ## /var/lib/kubelet/"
+    ssh $node "rm -rf /var/lib/kubelet/"
+
+    echo "## $node ## /var/lib/etcd/"
+    ssh $node "rm -rf /var/lib/etcd/"
+
+    echo "## $node ## clear iptables"
     ssh $node "iptables -F && iptables -X && \
                  iptables -t nat -F && iptables -t nat -X && \
                  iptables -t raw -F && iptables -t raw -X && \
              iptables -t mangle -F && iptables -t mangle -X"
-    echo "#### Restart crio $node ####"
+    echo "## $node ## Restart crio"
     ssh $node "systemctl restart crio"
 done
+
+if [ -d ${HOME}/.kube ] ; then
+    echo "## rm -rf ${HOME}/.kube"
+    rm -rf ${HOME}/.kube
+fi

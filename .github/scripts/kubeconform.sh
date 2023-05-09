bc0806a7f6aee31dc9641d88544f232d8c897d3c
Robert Love <terp4life2001@gmail.com>
2023-05-09 08:09:58 -0400

    fix-kubeconform

--- .github/scripts/kubeconform.sh
+++ .github/scripts/kubeconform.sh
@@ -24,10 +24,12 @@ kubeconform_args=(
     "-verbose"
 )
 
-echo "=== Validating standalone manifests in ${KUBERNETES_DIR}/crds ==="
-find "${KUBERNETES_DIR}/crds" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
+echo "=== Validating kustomizations in ${KUBERNETES_DIR}/crds ==="
+find "${KUBERNETES_DIR}/crds" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
-    kubeconform "${kubeconform_args[@]}" "${file}"
+    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
+    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
+      kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi

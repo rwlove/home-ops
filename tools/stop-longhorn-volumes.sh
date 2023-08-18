aab1e505f2f80434d79dfdaa984613b442e82172
Robert Love <terp4life2001@gmail.com>
2023-08-18 09:30:48 -0400

    fix-stop-longhorn

--- tools/stop-longhorn-volumes.sh
+++ tools/stop-longhorn-volumes.sh
@@ -3,14 +3,14 @@
 
 # Deployments
 kubectl -n home scale --replicas=0 deployment hajimari double-take nextcloud deepstack
-kubectl -n media scale --replicas=0 deployment gonic rompr stash youtubedl-material photoprism beets
+kubectl -n media scale --replicas=0 deployment gonic youtubedl-material
 kubectl -n downloads scale --replicas=0 deploy/qbittorrent
-kubectl -n monitoring scale --replicas=0 deployment statping portainer netdata-parent
+kubectl -n monitoring scale --replicas=0 deployment netdata-parent
 kubectl -n collab scale --replicas=0 deployment vikunja
 
 # Stateful Sets
-kubectl -n media scale --replicas=0 statefulsets whisparr lidarr sonarr radarr airsonic jellyfin sabnzbd
-kubectl -n home scale --replicas=0 statefulsets home-assistant emqx zwavejs2mqtt-z-stick-7 zwavejs2mqtt node-red esphome barcode-buddy grocy vaultwarden frigate zigbee2mqtt
+kubectl -n media scale --replicas=0 statefulsets lidarr sonarr radarr jellyfin sabnzbd
+kubectl -n home scale --replicas=0 statefulsets home-assistant emqx zwavejs2mqtt-z-stick-7 zwavejs2mqtt node-red esphome vaultwarden frigate zigbee2mqtt
 kubectl -n downloads scale --replicas=0 statefulsets prowlarr
 kubectl -n radio scale --replicas=0 statefulsets mopidy
 kubectl -n databases scale --replicas=0 statefulsets redis-master redis-replicas influxdb-influxdb2

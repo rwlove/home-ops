27c258f419ba00b8e7a38e679c6ed0335434d126
Robert Love <terp4life2001@gmail.com>
2023-08-30 14:58:37 -0400

    remove-worker9

--- init/create-cluster.sh
+++ init/create-cluster.sh
@@ -70,8 +70,8 @@ ssh root@worker7.thesteamedcrab.com rm -rf /var/lib/longhorn/*
 kubectl label nodes worker7.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
 ssh root@worker8.thesteamedcrab.com rm -rf /var/lib/longhorn/*
 kubectl label nodes worker8.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
-ssh root@worker9.thesteamedcrab.com rm -rf /var/lib/longhorn/*
-kubectl label nodes worker9.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
+#ssh root@worker9.thesteamedcrab.com rm -rf /var/lib/longhorn/*
+#kubectl label nodes worker9.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
 
 #kubectl label nodes worker2.thesteamedcrab.com "node.network/vlan=iot"
 kubectl label nodes worker7.thesteamedcrab.com "node.network/vlan=iot"

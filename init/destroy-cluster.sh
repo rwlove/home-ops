5a56ab149923509a63c94ca76ef6a56cba3e895b
Robert Love <terp4life2001@gmail.com>
2023-09-04 10:48:52 -0400

    ceph-cleanup-later

--- init/destroy-cluster.sh
+++ init/destroy-cluster.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-
 reset_cmd='kubeadm reset -f'
 
-./tools/destroy-rook-ceph-cluster.sh
+flux -n rook-ceph suspend hr rook-ceph-cluster
+flux -n rook-ceph suspend hr rook-ceph-operator
+
+kubectl patch cephblockpools.ceph.rook.io ceph-blockpool -n rook-ceph -p '{"metadata":{"finalizers":[]}}' --type=merge
+kubectl patch cephclusters.ceph.rook.io rook-ceph -n rook-ceph -p '{"metadata":{"finalizers":[]}}' --type=merge
+
+kubectl delete -n rook-ceph cephblockpool ceph-blockpool
+
+kubectl delete storageclasses.storage.k8s.io ceph-block ceph-bucket ceph-filesystem
+
+kubectl -n rook-ceph delete cephcluster rook-ceph
+
+kubectl -n rook-ceph wait --for=delete cephcluster rook-ceph
+
+kubectl -n rook-ceph delete hr rook-ceph-cluster
+kubectl -n rook-ceph delete hr rook-ceph-operator
 
 for node in worker1.thesteamedcrab.com \
         worker2.thesteamedcrab.com \
@@ -65,3 +79,17 @@ if [ -d ${HOME}/.kube ] ; then
     echo "## rm -rf ${HOME}/.kube/*"
     rm -rf ${HOME}/.kube/*
 fi
+
+for worker in worker5 worker6 worker7 worker8 ; do
+    echo "cleaning up ${worker}"
+    echo "- run /root/ceph-cleanup.sh"
+    ssh root@${worker} /root/ceph-cleanup.sh
+
+    echo "- dmsetup remove"
+    ssh root@${worker} ls /dev/mapper/ceph-* | xargs -I% -- dmsetup remove %
+
+    echo "- rm -rf /dev/ceph-* /dev/mapper/ceph--*"
+    ssh root@${worker} rm -rf /dev/ceph-* /dev/mapper/ceph--*
+done
+
+./tools/run-on-all-nodes.sh rm -rf /var/lib/rook/*

039ec53178141a7ae78cfea2a6a983da6be7556b
Robert Love <terp4life2001@gmail.com>
2023-09-04 12:18:44 -0400

    fix-prepare

--- init/prepare-cluster.sh
+++ init/prepare-cluster.sh
@@ -4,6 +4,7 @@ scp root@master1:~/.kube/config ~/.kube/config && \
 ./init/approve-csrs.sh && \
 kubectl kustomize --enable-helm bootstrap/cilium-quick-install | kubectl apply -f -
 
+ready=0
 all_nodes_ready() {
     if [ `kubectl get nodes | grep -c "NotReady"` -eq 0 ] ; then
 	ready=1

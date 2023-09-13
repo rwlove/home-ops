a0be1e15ffcb5fdbbdc39454d44e3b5259035932
Robert Love <terp4life2001@gmail.com>
2023-09-13 10:48:23 -0400

    Revert "skip-kube-proxy"
    This reverts commit 66bdc59f799dcf2d457a72eca01836a859a28366.

--- init/create-cluster.sh
+++ init/create-cluster.sh
@@ -22,8 +22,7 @@ chown -R ${USER}.${USER} ${HOME}/.kube
 
 certs=`kubeadm init phase upload-certs --upload-certs --config ./init/clusterconfiguration.yaml | tail -n 1`
 echo "certs: ${certs}"
-join_cmd=`kubeadm token create --print-join-command`
-worker_join_cmd="${join_cmd} --skip-phases=addon/kube-proxy"
+worker_join_cmd=`kubeadm token create --print-join-command`
 master_join_cmd="${worker_join_cmd} --control-plane --certificate-key ${certs}"
 
 #echo "XXXXXXXXXXX master_join_cmd START XXXXXXXXXXX"

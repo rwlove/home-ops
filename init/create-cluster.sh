dab32111511e129e8cb0115ce4b4f5855416f44f
Robert Love <terp4life2001@gmail.com>
2023-09-13 10:42:43 -0400

    cleanup-create-cluster

--- init/create-cluster.sh
+++ init/create-cluster.sh
@@ -59,13 +59,11 @@ for worker in worker1.thesteamedcrab.com \
 done
 
 # Configure Longhorn Disks (NVMe Drives) -- see README hardware section
-echo " Label workers 1, 2, 3, 5, 6, 7 and 8 for longhorn since they have NVMe drives"
+echo " Label workers 1, 2, 5, 6, 7 and 8 for longhorn since they have NVMe drives"
 ssh root@worker1.thesteamedcrab.com rm -rf /var/lib/longhorn/*
 kubectl label nodes worker1.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
 ssh root@worker2.thesteamedcrab.com rm -rf /var/lib/longhorn/*
 kubectl label nodes worker2.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
-#ssh root@worker3.thesteamedcrab.com rm -rf /var/lib/longhorn/*
-#kubectl label nodes worker3.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
 ssh root@worker5.thesteamedcrab.com rm -rf /var/lib/longhorn/*
 kubectl label nodes worker5.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
 ssh root@worker6.thesteamedcrab.com rm -rf /var/lib/longhorn/*
@@ -74,10 +72,7 @@ ssh root@worker7.thesteamedcrab.com rm -rf /var/lib/longhorn/*
 kubectl label nodes worker7.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
 ssh root@worker8.thesteamedcrab.com rm -rf /var/lib/longhorn/*
 kubectl label nodes worker8.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
-#ssh root@worker9.thesteamedcrab.com rm -rf /var/lib/longhorn/*
-#kubectl label nodes worker9.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
 
-#kubectl label nodes worker2.thesteamedcrab.com "node.network/vlan=iot"
 kubectl label nodes worker7.thesteamedcrab.com "node.network/vlan=iot"
 kubectl label nodes worker8.thesteamedcrab.com "node.network/vlan=iot"
 #kubectl label nodes worker3.thesteamedcrab.com "node.network/vlan=security"

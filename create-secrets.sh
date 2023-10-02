a4bbf4640246af726b9b9c6ae2da29542d866cc1
Robert Love <terp4life2001@gmail.com>
2023-10-02 16:44:16 -0400

    update-secrets

--- create-secrets.sh
+++ create-secrets.sh
@@ -106,8 +106,8 @@ envsubst < ./clusters/lovenet/apps/home/smtp-relay/app/secrets-tmpl.yaml > ./clu
 sops --encrypt --in-place ./clusters/lovenet/apps/home/smtp-relay/app/secrets.yaml
 
 echo "Create K8SGPT Secrets"
-envsubst < ./clusters/lovenet/apps/kube-system/k8sgpt/config/secrets-tmpl.yaml > ./clusters/lovenet/apps/kube-system/k8sgpt/config/secrets.yaml
-sops --encrypt --in-place ./clusters/lovenet/apps/kube-system/k8sgpt/config/secrets.yaml
+envsubst < ./clusters/lovenet/apps/ai/k8sgpt/config/secrets-tmpl.yaml > ./clusters/lovenet/apps/ai/k8sgpt/config/secrets.yaml
+sops --encrypt --in-place ./clusters/lovenet/apps/ai/k8sgpt/config/secrets.yaml
 
 echo "Create NextCloud Secrets"
 envsubst < ./clusters/lovenet/apps/home/nextcloud/app/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/nextcloud/app/secrets.yaml

c6b4b253485e52ab6d913e257749b37d48086a80
Robert Love <terp4life2001@gmail.com>
2023-10-17 17:05:18 -0400

    frigate-external-secrets

--- create-secrets.sh
+++ create-secrets.sh
@@ -8,10 +8,6 @@ echo "Create Cluster Secrets"
 envsubst < ./tmpl/cluster-secrets.yaml > ./clusters/lovenet/base/cluster-secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 
-echo "Create Frigate Secrets"
-envsubst < ./clusters/lovenet/apps/home/frigate/app/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/frigate/app/secrets.yaml
-sops --encrypt --in-place ./clusters/lovenet/apps/home/frigate/app/secrets.yaml
-
 echo "Create Double-Take Secrets"
 envsubst < ./clusters/lovenet/apps/home/double-take/app/secrets-tmpl.yaml > ./clusters/lovenet/apps/home/double-take/app/secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/apps/home/double-take/app/secrets.yaml

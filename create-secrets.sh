20ffb730a6529de7f494297fffcbe5d4e5c47421
Robert Love <terp4life2001@gmail.com>
2023-10-17 20:17:52 -0400

    cleanup-create-secrets

--- create-secrets.sh
+++ create-secrets.sh
@@ -8,10 +8,6 @@ echo "Create Cluster Secrets"
 envsubst < ./tmpl/cluster-secrets.yaml > ./clusters/lovenet/base/cluster-secrets.yaml
 sops --encrypt --in-place ./clusters/lovenet/base/cluster-secrets.yaml
 
-echo "Create Github Notification Secrets"
-envsubst < ./clusters/lovenet/apps/notifications/github/secrets-tmpl.yaml > ./clusters/lovenet/apps/notifications/github/secrets.yaml
-sops --encrypt --in-place ./clusters/lovenet/apps/notifications/github/secrets.yaml
-
 echo "Create QBittorrent-rss Secrets"
 #sops --encrypt ./clusters/lovenet/apps/media/qbittorrent-rss/app/feeds-json-secrets-tmpl.yaml > ./clusters/lovenet/apps/media/qbittorrent-rss/app/feeds-json-secrets.yaml
 #sops --encrypt ./clusters/lovenet/apps/media/qbittorrent-rss/app/env-secrets-tmpl.yaml > ./clusters/lovenet/apps/media/qbittorrent-rss/app/env-secrets.yaml

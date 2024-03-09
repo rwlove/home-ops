73d72e35b14ad09227f09e423ec5d0b7b17aed44
Robert Love <terp4life2001@gmail.com>
2024-03-09 13:15:21 -0500

    update-kube-vip

--- init/kube-vip.sh
+++ init/kube-vip.sh
@@ -5,6 +5,6 @@ export INTERFACE=eno1
 
 mkdir -p /etc/kubernetes/manifests/
 
-#podman run --network host --rm plndr/kube-vip:v0.6.0 manifest pod --interface $INTERFACE --vip $VIP --controlplane --services --arp --leaderElection | tee /etc/kubernetes/manifests/kube-vip.yaml
+podman run --network host --rm plndr/kube-vip:v0.7.1 manifest pod --interface $INTERFACE --vip $VIP --controlplane --services --arp --leaderElection | tee /etc/kubernetes/manifests/kube-vip.yaml
 
-podman run --network host --rm ghcr.io/kube-vip/kube-vip:v0.7.1 manifest pod --interface $INTERFACE --vip $VIP --controlplane --services --arp --leaderElection | tee /etc/kubernetes/manifests/kube-vip.yaml
+#podman run --network host --rm ghcr.io/kube-vip/kube-vip:v0.7.1 manifest pod --interface $INTERFACE --vip $VIP --controlplane --services --arp --leaderElection | tee /etc/kubernetes/manifests/kube-vip.yaml

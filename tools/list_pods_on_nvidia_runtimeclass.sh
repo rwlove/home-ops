39bf83fee7d7de8b143612b1186e96e1b2627b82
Robert Love <terp4life2001@gmail.com>
2024-12-31 14:57:27 -0500

    try-gpu-operator

--- tools/list_pods_on_nvidia_runtimeclass.sh
+++ tools/list_pods_on_nvidia_runtimeclass.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-kubectl get pods -A -o jsonpath='{.items[?(@.spec.runtimeClassName=="nvidia")].metadata.name}' | tr ' ' '\n'
+#kubectl get pods -A -o jsonpath='{.items[?(@.spec.runtimeClassName=="nvidia")].metadata.name}' | tr ' ' '\n'
+
+for pod in `kubectl get pods -A -o jsonpath='{.items[?(@.spec.runtimeClassName=="nvidia")].metadata.name}'` ; do
+    kubectl get pods -A -o wide | grep $pod
+done
 
 echo

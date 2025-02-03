31297c7dc9c07455c73abc9d8cd82f02ca467c33
Robert Love <terp4life2001@gmail.com>
2025-02-03 12:06:43 -0500

    add-pull-models

---
+++ tools/ollama-pull-models.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+curl https://ollama.thesteamedcrab.com/api/pull -d '{
+  "name": "deepseek-r1:8b"
+}'
+
+curl https://ollama.thesteamedcrab.com/api/pull -d '{
+  "name": "llava-llama3:8b"
+}'
+
+#curl https://ollama.thesteamedcrab.com/api/pull -d '{
+#  "name": "llama2-uncensored:latest"
+#}'
+
+#curl https://ollama.thesteamedcrab.com/api/pull -d '{
+#  "name": "llama2"
+#}'
+
+#curl https://ollama.thesteamedcrab.com/api/pull -d '{
+#  "name": "llama3.1"
+#}'
+
+#curl https://ollama.thesteamedcrab.com/api/pull -d '{
+#  "name": "dolphin-mixtral"
+#}'
+
+#curl https://ollama.thesteamedcrab.com/api/pull -d '{
+#  "name": "acon96/Home-3B-v3-GGUF"
+#}'
+
+#curl https://ollama.thesteamedcrab.com/api/pull -d '{
+#  "name": "mxbai-embed-large"
+#}'
+
+# Obsidian Excalidraw: dall-e-3
+#curl https://ollama.thesteamedcrab.com/api/pull -d '{
+#  "name": "ehristoforu/dalle-3-xl-v2"
+#}'
+
+# Obsidian Excalidraw: gpt-3.5-turbo-1106
+#curl https://ollama.thesteamedcrab.com/api/pull -d '{
+#  "name": "gpt-3.5-turbo"
+#}'
+
+# Obsidian Excalidraw: gpt-4-vision-preview
+#curl https://ollama.thesteamedcrab.com/api/pull -d '{
+#  "name": "playground_with_gpt-4-vision-preview"
+#}'
+
+curl https://ollama.thesteamedcrab.com/api/tags
+
+#curl https://ollama.thesteamedcrab.com/api/generate -d '{
+#  "model": "llama2",
+#  "prompt": "Why is the sky blue?"
+#}'

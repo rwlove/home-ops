6abbc17a40c5d72f04e7c30f1d25c83ee08a5862
Robert Love <terp4life2001@gmail.com>
2025-04-22 09:39:48 -0400

    add-echo

--- kubernetes/main/apps/home/birdcam-youtube/app/start.sh
+++ kubernetes/main/apps/home/birdcam-youtube/app/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+echo "script start"
+echo "YOUTUBE_KEY is ${YOUTUBE_KEY}"
+
 /bin/ffmpeg > -re -i rtsp://frigate.home:8554/birdcam -f lavfi -t 3600 -i anullsrc=r=44100:cl=stereo -vcodec libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -keyint_min 25 -g 50 -acodec aac -ar 44100 -b:a 128k -f flv rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_KEY}
+
+echo "script end"

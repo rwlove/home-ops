3ad7898f17ac0b3cebe6a505f64793c395f6cedf
Robert Love <terp4life2001@gmail.com>
2025-08-06 11:28:55 -0400

    update

--- init/create-cluster.sh
+++ init/create-cluster.sh
@@ -69,9 +69,9 @@ ssh root@worker4.thesteamedcrab.com rm -rf /var/lib/longhorn/*
 kubectl label nodes worker4.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
 ssh root@worker5.thesteamedcrab.com rm -rf /var/lib/longhorn/*
 kubectl label nodes worker5.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
-#ssh root@worker6.thesteamedcrab.com rm -rf /var/lib/longhorn/*
-#kubectl label nodes worker6.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
+ssh root@worker6.thesteamedcrab.com rm -rf /var/lib/longhorn/*
+kubectl label nodes worker6.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
 ssh root@worker7.thesteamedcrab.com rm -rf /var/lib/longhorn/*
 kubectl label nodes worker7.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
 ssh root@worker8.thesteamedcrab.com rm -rf /var/lib/longhorn/*
-kubectl label nodes worker8.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"
+#kubectl label nodes worker8.thesteamedcrab.com "node.longhorn.io/create-default-disk=true"

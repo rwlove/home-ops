c1c10c891441ea04d2282bb58d281903b333e151
Robert Love <terp4life2001@gmail.com>
2025-08-16 18:00:13 -0400

    use-atuin-db

--- tools/check-postgres-dbs.sh
+++ tools/check-postgres-dbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DBS="postgres postgres-immich postgres-home-assistant postgres-pocket-id postgres-lldap postgres-paperless"
+DBS="postgres postgres-immich postgres-home-assistant postgres-pocket-id postgres-lldap postgres-paperless postgres-atuin"
 
 for db in $DBS ; do
 

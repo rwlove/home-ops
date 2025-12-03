6a804e5827f10f5d1fef5d1e620ab802d70e3647
Robert Love <terp4life2001@gmail.com>
2025-12-02 19:46:50 -0500

    update

--- init/kube-vip.sh
+++ init/kube-vip.sh
@@ -5,7 +5,7 @@ export INTERFACE=enp0s31f6
 
 mkdir -p /etc/kubernetes/manifests/
 
-podman run --network host --rm ghcr.io/kube-vip/kube-vip:v1.0.2 manifest pod --interface $INTERFACE --vip $VIP --cidr "32" --controlplane --services --arp --leaderElection | tee /etc/kubernetes/manifests/kube-vip.yaml
+podman run --network host --rm ghcr.io/kube-vip/kube-vip:v1.0.2 manifest pod --interface $INTERFACE --vip $VIP --vipSubnet "/32" --controlplane --services --arp --leaderElection | tee /etc/kubernetes/manifests/kube-vip.yaml
 
 sed -i 's#path: /etc/kubernetes/admin.conf#path: /etc/kubernetes/super-admin.conf#' /etc/kubernetes/manifests/kube-vip.yaml
 

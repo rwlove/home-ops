faaa812ceedb59fff182333348301178ed35f6d2
Robert Love <terp4life2001@gmail.com>
2025-12-03 14:33:04 -0500

    homelab-secrets

--- init/initialize-cluster.sh
+++ init/initialize-cluster.sh
@@ -15,11 +15,11 @@ kubectl apply -f ./kubernetes/main/apps/namespaces/network.yaml
 echo "# Create Secrets Patch and Push"
 ./create-secrets.sh
 
-echo "Create Cluster Settings Configmap"
-kubectl apply -f ./kubernetes/main/flux/vars/cluster-settings.yaml
+#echo "Create Cluster Settings Configmap"
+#kubectl apply -f ./kubernetes/main/flux/vars/cluster-settings.yaml
 
-echo "Create Cluster Secrets"
-sops --decrypt ./kubernetes/main/flux/vars/cluster-secrets.yaml | kubectl apply -f -
+#echo "Create Cluster Secrets"
+#sops --decrypt ./kubernetes/main/flux/vars/cluster-secrets.yaml | kubectl apply -f -
 
 echo "Apply CRDS"
 helmfile -f "bootstrap/helmfile.d/00-crds.yaml" template -q | kubectl apply --server-side --field-manager bootstrap --force-conflicts -f -

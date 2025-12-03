101ad1be420bc9d219bd66fec913ea7483eec058
Robert Love <terp4life2001@gmail.com>
2025-12-03 15:31:21 -0500

    fix

--- init/initialize-cluster.sh
+++ init/initialize-cluster.sh
@@ -15,11 +15,8 @@ kubectl apply -f ./kubernetes/main/apps/namespaces/network.yaml
 echo "# Create Secrets Patch and Push"
 ./create-secrets.sh
 
-#echo "Create Cluster Settings Configmap"
-#kubectl apply -f ./kubernetes/main/flux/vars/cluster-settings.yaml
-
-#echo "Create Cluster Secrets"
-#sops --decrypt ./kubernetes/main/flux/vars/cluster-secrets.yaml | kubectl apply -f -
+echo "Create Cluster Settings Configmap"
+kubectl apply -f ./kubernetes/main/apps/flux-system/flux-instance/app/cluster-settings.yaml
 
 echo "Apply CRDS"
 helmfile -f "bootstrap/helmfile.d/00-crds.yaml" template -q | kubectl apply --server-side --field-manager bootstrap --force-conflicts -f -

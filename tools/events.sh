c6ee406d2deac34ae2533f524e90f54d7a0e82b4
Robert Love <terp4life2001@gmail.com>
2026-05-02 11:51:14 -0400

    remove-filter

--- tools/events.sh
+++ tools/events.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-kubectl get events -A --watch-only | grep -vE 'ArtifactUpToDate|ReconciliationSucceeded|GitOperationSucceeded|DependencyNotReady|ImageGCFailed|CacheOperationFailed|NotificationDispatchFailed'
+#kubectl get events -A --watch-only | grep -vE 'ArtifactUpToDate|ReconciliationSucceeded|GitOperationSucceeded|DependencyNotReady|ImageGCFailed|CacheOperationFailed|NotificationDispatchFailed'
+
+kubectl get events -A --watch-only | grep -vE 'ArtifactUpToDate|ReconciliationSucceeded|GitOperationSucceeded|DependencyNotReady'
